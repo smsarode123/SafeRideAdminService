c1d24cb9a33ece693562fbd9e7ace0a60fd3187f
smssarode123 <sagarsarode1997@gmail.com>
2024-08-21 12:18:52 +0530

    added @service annotation

--- InsuranceAdminService/src/main/java/com/insurance/adminservice/serviceimpl/ServiceCenterImpl.java
+++ InsuranceAdminService/src/main/java/com/insurance/adminservice/serviceimpl/ServiceCenterImpl.java
@@ -1,11 +1,12 @@
 package com.insurance.adminservice.serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.insurance.adminservice.model.ServiceCenter;
 import com.insurance.adminservice.repository.ServiceCenterRepository;
 import com.insurance.adminservice.servicei.ServiceCenterService;
-
+@Service
 public class ServiceCenterImpl implements ServiceCenterService{
 	
 	
